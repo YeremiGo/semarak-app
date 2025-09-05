a8187241753fbcb5201edb59de970ead6d1dae9d
Yeremi Purba <yeremikornelius2005@gmail.com>
2025-09-05 21:18:31 +0800

    Mengubah route login menjadi route utama

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\LaporanController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 // Rute menampilkan formulir
